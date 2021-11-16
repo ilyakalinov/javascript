7598654771ec5dca2f61edc3d0fa51ef904d4088
ilya <ilya_wot2012@gmail.com>
2021-11-16 16:51:33 +0300

    lesson40

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,31 @@
 // ////////////////////////////////////////////////////////////
+// 040 Работа с датами
+// ////////////////////////////////////////////////////////////
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+     let some = i ** 3;
+}
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);
+////////////////////////////////////////////////////
+// const now = new Date('2021-11-15');
+
+// console.log(now.setHours(18, 40));
+// console.log(now);
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+
+// console.log(now.getHours());
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+// console.log(now.getTime());
+
+// ////////////////////////////////////////////////////////////
 // 037 События на мобильных устройствах
 // ////////////////////////////////////////////////////////////
 const btns = document.querySelectorAll('button'),
